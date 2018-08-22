6f6c6c2fe3eba58754f7fe364a3bd18d5125932e
Ivan <ivan.palii@outlook.com>
2018-08-22 18:00:59 +0300

    Change from dotnetstandart to core

---
+++ Social-Manager.Abstraction/Interfaces/TEnity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Social_Manager.Abstraction.Interfaces
+{
+    public interface TEnity
+    {
+        public int Id { get; set; }
+    }
+}
