6ac01c03041a6433bebb5fb40af15d8dd5e2288d
Ivan <ivan.palii@outlook.com>
2018-11-21 17:19:38 +0200

    Rename Project Name

--- Stream-G.Abstraction/TwitchSerices/TwitchUserService.cs
+++ Stream-G.Abstraction/TwitchSerices/TwitchUserService.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using StreamG.DTO;
-using StreamG.Core.Domain;
+
 using StreamG.Services.Interfaces;
 
 namespace StreamG.Services.TwitchSerices
