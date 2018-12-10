c582c996687d841cd1a3818fd3086e9318c93769
Ivan <ivan.palii@outlook.com>
2018-12-10 19:05:37 +0200

    Restore services

---
+++ Stream-G.Abstraction/TwitchSerices/TwitchAuthorizationService.cs
@@ -0,0 +1,30 @@
+﻿using StreamG.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Twitch.Api.Abstraction;
+using Twitch.API;
+using Twitch.API.Data.Token;
+
+namespace StreamG.Services.TwitchSerices
+{
+    public class TwitchAuthorizationService : ITwitchAuthorizationService
+    {
+        ITwitchAuthorizing _twitchAuthorizing;
+
+        public TwitchAuthorizationService(ITwitchAuthorizing twitchAuthorizing)
+        {
+            _twitchAuthorizing = twitchAuthorizing;
+        }
+        public Token GetApplicationToken()
+        {
+            var token = _twitchAuthorizing.GetAuthorizeToken();
+            return token;
+        }
+
+        public void SetAuhorizeCode(string code)
+        {
+            
+        }
+    }
+}
---
+++ Stream-G.Abstraction/TwitchSerices/TwitchUserService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using StreamG.Services.Interfaces;
+using Twitch.Api.Abstraction;
+using Twitch.API;
+using Twitch.API.Data.User;
+
+namespace StreamG.Services.TwitchSerices
+{
+    public class TwitchUserService : ITwitchUserService
+    {
+        private ITwitchUserApi _twitchUserApi;
+
+        public TwitchUserService(ITwitchUserApi twitchUserApi)
+        {
+            _twitchUserApi = twitchUserApi;
+        }
+
+        public TwitchUser GetUser()
+        {
+            return _twitchUserApi.GetUserData();
+        }
+    }
+}
